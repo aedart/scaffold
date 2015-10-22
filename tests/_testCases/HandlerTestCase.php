9e33c1a6bab36fc772e255ec7203ae90799c0e6e
Alin Eugen Deac <aedart@gmail.com>
2015-10-22 14:49:22 +0200

    Started working on a common test-case for handlers

---
+++ tests/_testCases/HandlerTestCase.php
@@ -0,0 +1,16 @@
+<?php
+
+use Aedart\Testing\Laravel\TestCases\unit\UnitTestCase;
+
+/**
+ * Handler Test Case
+ *
+ * @author Alin Eugen Deac <aedart@gmail.com>
+ */
+class HandlerTestCase extends UnitTestCase{
+
+    /***********************************************************
+     * Helpers and utilities
+     **********************************************************/
+
+}
