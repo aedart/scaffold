a7954361b8b266b4d2dbc43d54c84d09e7209d9a
Alin Eugen Deac <aedart@gmail.com>
2015-10-22 14:50:12 +0200

    Configuration for testing a file handler

---
+++ tests/_data/handlers/files/fileHandler/scaffold.php
@@ -0,0 +1,31 @@
+<?php
+
+return [
+
+    'basePath' => __DIR__ . 'templates',
+
+    'files' => [
+            'composer.json.twig'    => 'Aedart/Scaffold/Handlers/FileHandler::class',
+    ],
+
+    'data'      => [
+
+        'name'      => [
+            'ask'           =>  true,
+            'description'   =>  'Name of the project',
+        ],
+
+        'author_name'  => [
+            'ask'           =>  true,
+            'description'   =>  'Author\'s name',
+            'default'       =>  'Alin Eugen Deac'
+        ],
+
+        'author_email'  => [
+            'ask'           =>  false,
+            'default'       =>  'aedart@gmail.com'
+        ],
+
+    ],
+
+];
