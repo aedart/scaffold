21178035d475191fdf1109598dd7bfb6ac88f0c7
Alin Eugen Deac <aedart@gmail.com>
2015-10-24 11:55:06 +0200

    Using model traits

--- src/Engines/TwigEngine.php
+++ src/Engines/TwigEngine.php
@@ -1,5 +1,7 @@
 <?php namespace Aedart\Scaffold\Engines;
 
+use Aedart\Model\Traits\Arrays\DataTrait;
+use Aedart\Model\Traits\Strings\BasePathTrait;
 use Twig_Environment;
 use Twig_Loader_Filesystem;
 
@@ -10,6 +12,8 @@ use Twig_Loader_Filesystem;
  * @package Aedart\Scaffold\Engines
  */
 class TwigEngine {
+
+    use DataTrait, BasePathTrait;
 
     /**
      * The engine instance
@@ -49,73 +53,6 @@ class TwigEngine {
         $template = $this->twig->loadTemplate($this->getTemplateFile());
 
         return $template->render($this->getData());
-    }
-
-    /**************************************************
-     * TODO: Data - split into trait
-     **************************************************/
-
-    /**
-     * Data to be parsed and rendered by the template
-     *
-     * @var array|null
-     */
-    protected $data = null;
-
-    /**
-     * Set the given data
-     *
-     * @param array $data Data to be parsed and rendered by the template
-     *
-     * @return void
-     */
-    public function setData(array $data) {
-        $this->data = $data;
-    }
-
-    /**
-     * Get the given data
-     *
-     * If no data has been set, this method will
-     * set and return a default data, if any such
-     * value is available
-     *
-     * @see getDefaultData()
-     *
-     * @return array|null data or null if none data has been set
-     */
-    public function getData() {
-        if (!$this->hasData() && $this->hasDefaultData()) {
-            $this->setData($this->getDefaultData());
-        }
-        return $this->data;
-    }
-
-    /**
-     * Get a default data value, if any is available
-     *
-     * @return array|null A default data value or Null if no default value is available
-     */
-    public function getDefaultData() {
-        return null;
-    }
-
-    /**
-     * Check if data has been set
-     *
-     * @return bool True if data has been set, false if not
-     */
-    public function hasData() {
-        return !is_null($this->data);
-    }
-
-    /**
-     * Check if a default data is available or not
-     *
-     * @return bool True of a default data is available, false if not
-     */
-    public function hasDefaultData() {
-        return !is_null($this->getDefaultData());
     }
 
     /**************************************************
@@ -183,72 +120,5 @@ class TwigEngine {
      */
     public function hasDefaultTemplateFile() {
         return !is_null($this->getDefaultTemplateFile());
-    }
-
-    /**************************************************
-     * TODO: Base Path - split into trait
-     **************************************************/
-
-    /**
-     * Base Path; location of where the templates are located
-     *
-     * @var string|null
-     */
-    protected $basePath = null;
-
-    /**
-     * Set the given base path
-     *
-     * @param string $path Base Path; location of where the templates are located
-     *
-     * @return void
-     */
-    public function setBasePath($path) {
-        $this->basePath = $path;
-    }
-
-    /**
-     * Get the given base path
-     *
-     * If no base path has been set, this method will
-     * set and return a default base path, if any such
-     * value is available
-     *
-     * @see getDefaultBasePath()
-     *
-     * @return string|null base path or null if none base path has been set
-     */
-    public function getBasePath() {
-        if (!$this->hasBasePath() && $this->hasDefaultBasePath()) {
-            $this->setBasePath($this->getDefaultBasePath());
-        }
-        return $this->basePath;
-    }
-
-    /**
-     * Get a default base path value, if any is available
-     *
-     * @return string|null A default base path value or Null if no default value is available
-     */
-    public function getDefaultBasePath() {
-        return null;
-    }
-
-    /**
-     * Check if base path has been set
-     *
-     * @return bool True if base path has been set, false if not
-     */
-    public function hasBasePath() {
-        return !is_null($this->basePath);
-    }
-
-    /**
-     * Check if a default base path is available or not
-     *
-     * @return bool True of a default base path is available, false if not
-     */
-    public function hasDefaultBasePath() {
-        return !is_null($this->getDefaultBasePath());
     }
 }
