74ca039fb6f7843f344eea7063bb0a038429d206
Alin Eugen Deac <aedart@gmail.com>
2015-10-24 14:18:30 +0200

    A test-case for DTOs

---
+++ tests/_testCases/DtoTestCase.php
@@ -0,0 +1,15 @@
+<?php
+use Aedart\Testing\Laravel\TestCases\unit\UnitWithLaravelTestCase;
+
+/**
+ * Class DtoTestCase
+ *
+ * @author Alin Eugen Deac <aedart@gmail.com>
+ */
+class DtoTestCase extends UnitWithLaravelTestCase{
+
+    /***********************************************************
+     * Helpers and utilities
+     **********************************************************/
+
+}
