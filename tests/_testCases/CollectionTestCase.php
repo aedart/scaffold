8c3ab3551b5b95abdc08a089567202f9fa274b4a
Alin Eugen Deac <aedart@gmail.com>
2015-10-25 09:41:10 +0100

    Test-case for collections

---
+++ tests/_testCases/CollectionTestCase.php
@@ -0,0 +1,15 @@
+<?php
+use Aedart\Testing\Laravel\TestCases\unit\UnitWithLaravelTestCase;
+
+/**
+ * Class CollectionTestCase
+ *
+ * @author Alin Eugen Deac <aedart@gmail.com>
+ */
+abstract class CollectionTestCase extends UnitWithLaravelTestCase{
+
+    /***********************************************************
+     * Helpers and utilities
+     **********************************************************/
+
+}
