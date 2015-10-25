3db78242ff7a6e93eb9e6945cdf295d7e69a63ee
Alin Eugen Deac <aedart@gmail.com>
2015-10-25 13:04:51 +0100

    Added the collections and the Scaffold DTO to the service provider's registrations

--- src/Providers/ScaffoldServiceProvider.php
+++ src/Providers/ScaffoldServiceProvider.php
@@ -1,6 +1,12 @@
 <?php namespace Aedart\Scaffold\Providers;
 
 use Illuminate\Support\ServiceProvider;
+use Aedart\Scaffold\Contracts\Collections\AskablePropertiesCollection as AskablePropertiesCollectionInterface;
+use Aedart\Scaffold\Collections\AskablePropertiesCollection;
+use Aedart\Scaffold\Contracts\Collections\TemplatesCollection as TemplatesCollectionInterface;
+use Aedart\Scaffold\Collections\TemplatesCollection;
+use Aedart\Scaffold\Contracts\Data\Scaffold as ScaffoldInterface;
+use Aedart\Scaffold\Data\Scaffold;
 use Aedart\Scaffold\Contracts\Data\Template as TemplateInterface;
 use Aedart\Scaffold\Data\Template;
 use Aedart\Scaffold\Contracts\Data\AskableProperty as AskablePropertyInterface;
@@ -22,6 +28,9 @@ class ScaffoldServiceProvider extends ServiceProvider{
     public function register() {
         $this->registerAskableProperty();
         $this->registerTemplate();
+        $this->registerAskablePropertiesCollection();
+        $this->registerTemplatesCollection();
+        $this->registerScaffold();
     }
 
     /******************************************************
@@ -43,6 +52,33 @@ class ScaffoldServiceProvider extends ServiceProvider{
     public function registerTemplate() {
         $this->app->bind(TemplateInterface::class, function($app, array $data = []){
             return new Template($data);
+        });
+    }
+
+    /**
+     * Register the scaffold (dto)
+     */
+    public function registerScaffold() {
+        $this->app->bind(ScaffoldInterface::class, function($app, array $data = []){
+            return new Scaffold($data);
+        });
+    }
+
+    /**
+     * Register the askable-properties collection
+     */
+    public function registerAskablePropertiesCollection() {
+        $this->app->bind(AskablePropertiesCollectionInterface::class, function($app, array $data = []){
+            return new AskablePropertiesCollection($data);
+        });
+    }
+
+    /**
+     * Register the templates collection
+     */
+    public function registerTemplatesCollection() {
+        $this->app->bind(TemplatesCollectionInterface::class, function($app, array $data = []){
+            return new TemplatesCollection($data);
         });
     }
 }
