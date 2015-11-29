a9b759cfea750ae737ab5426189fdc879058b05f
Alin Eugen Deac <aedart@gmail.com>
2015-11-29 13:02:29 +0100

    Started work on the Template Handler interface

---
+++ src/Contracts/Handlers/TemplateHandler.php
@@ -0,0 +1,17 @@
+<?php namespace Aedart\Scaffold\Contracts\Handlers;
+
+use Aedart\Model\Contracts\Arrays\DataAware;
+
+/**
+ * <h1>Interface Template-Handler</h1>
+ *
+ * TODO...
+ *
+ * @author Alin Eugen Deac <aedart@gmail.com>
+ * @package Aedart\Scaffold\Contracts\Handlers
+ */
+interface TemplateHandler extends FileHandler, DataAware{
+
+    // TODO: "Template" Engine aware
+
+}
