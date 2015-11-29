c424fb2ae571bbb7cacda98e211c51b317ea9cbb
Alin Eugen Deac <aedart@gmail.com>
2015-11-29 14:14:06 +0100

    A base test-case for engine tests

---
+++ tests/_testCases/EngineTestCase.php
@@ -0,0 +1,16 @@
+<?php
+
+use Aedart\Testing\Laravel\TestCases\unit\UnitTestCase;
+
+/**
+ * Engine Test Case
+ *
+ * @author Alin Eugen Deac <aedart@gmail.com>
+ */
+abstract class EngineTestCase extends UnitTestCase{
+
+    /***********************************************************
+     * Helpers and utilities
+     **********************************************************/
+
+}
