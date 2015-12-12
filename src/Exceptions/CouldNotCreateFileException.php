376800c816ca9ee4fe46916d4650a338b293e536
Alin Eugen Deac <aedart@gmail.com>
2015-12-12 14:51:26 +0100

    Completed the Template Handler implementation

---
+++ src/Exceptions/CouldNotCreateFileException.php
@@ -0,0 +1,15 @@
+<?php namespace Aedart\Scaffold\Exceptions;
+
+use RuntimeException;
+
+/**
+ * Could Not Create File Exception
+ *
+ * Throw this exception when a file could not be created
+ *
+ * @author Alin Eugen Deac <aedart@gmail.com>
+ * @package Aedart\Scaffold\Exceptions
+ */
+class CouldNotCreateFileException extends RuntimeException{
+
+}
