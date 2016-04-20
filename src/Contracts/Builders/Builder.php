6fe119ecb2e593ac60176ad06e2e4a87a6ba0b96
Alin Eugen Deac <aedart@gmail.com>
2016-04-20 07:35:09 +0200

    Add builder description

--- src/Contracts/Builders/Builder.php
+++ src/Contracts/Builders/Builder.php
@@ -1,7 +1,12 @@
 <?php namespace Aedart\Scaffold\Contracts\Builders;
 
 /**
- * Interface Builder
+ * Builder
+ *
+ * A scaffold builder is responsible for creating a
+ * specified directory structure, copy files into
+ * the project folder and generate files based on
+ * available templates.
  *
  * @author Alin Eugen Deac <aedart@gmail.com>
  * @package Aedart\Scaffold\Contracts\Builders
