dced751e474c1565928f98489540f28f3164d5d0
Alin Eugen Deac <aedart@gmail.com>
2016-04-20 09:58:38 +0200

    Add Directories Collection interface

---
+++ src/Contracts/Collections/Directories.php
@@ -0,0 +1,23 @@
+<?php namespace Aedart\Scaffold\Contracts\Collections;
+
+use Aedart\Util\Interfaces\Collections\IPartialCollection;
+
+/**
+ * Directories
+ *
+ * A collection of directory paths
+ *
+ * @author Alin Eugen Deac <aedart@gmail.com>
+ * @package Aedart\Scaffold\Contracts\Collections
+ */
+interface Directories extends IPartialCollection
+{
+    /**
+     * Add a directory path
+     *
+     * @param string $path Relative or absolute path to a directory
+     *
+     * @return bool
+     */
+    public function add($path);
+}
