56fef7ce7398bfcfe7fe160a436d9688a3577d89
Alin Eugen Deac <aedart@gmail.com>
2016-04-20 14:59:53 +0200

    Add Build console command
    The core command of the scaffold application

---
+++ src/Console/BuildCommand.php
@@ -0,0 +1,35 @@
+<?php namespace Aedart\Scaffold\Console;
+
+use Symfony\Component\Console\Command\Command;
+use Symfony\Component\Console\Input\InputArgument;
+use Symfony\Component\Console\Input\InputOption;
+
+/**
+ * Build Command
+ *
+ * Builds folders and files, based on the given configuration
+ *
+ * @author Alin Eugen Deac <aedart@gmail.com>
+ * @package Aedart\Scaffold\Console
+ */
+class BuildCommand extends BaseCommand
+{
+    protected function configure()
+    {
+        $this
+            ->setName('build')
+            ->setDescription('Creates folders, copies and generate files into the given output path')
+            ->addArgument('config', InputArgument::REQUIRED, 'Path to the scaffold configuration file')
+            ->addOption('output', null, InputOption::VALUE_OPTIONAL, 'Path where to build the scaffold folders and files', getcwd());
+    }
+
+    /**
+     * Execute the command
+     *
+     * @return int|null
+     */
+    public function runCommand()
+    {
+        
+    }
+}
