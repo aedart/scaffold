b463cfb0f2c271d36a6e1213c775f2f08862656c
Alin Eugen Deac <aedart@gmail.com>
2016-04-20 12:07:20 +0200

    Add collection, filesystem and output path utils

--- tests/_testCases/BaseUnitTest.php
+++ tests/_testCases/BaseUnitTest.php
@@ -1,8 +1,12 @@
 <?php
 
+use Aedart\Scaffold\Contracts\Collections\Directories;
+use Codeception\Configuration;
 use Codeception\TestCase\Test;
+use Codeception\Util\Debug;
 use Faker\Factory;
 use Illuminate\Contracts\Logging\Log;
+use Illuminate\Filesystem\Filesystem;
 use Mockery as m;
 
 /**
@@ -28,7 +32,15 @@ abstract class BaseUnitTest extends Test
 
     protected function _before()
     {
+        // Faker instance
         $this->faker = Factory::create();
+
+        // Create output path if it does not exist
+        if(!file_exists($this->outputPath())){
+            mkdir($this->outputPath(), 0755, true);
+
+            Debug::debug(sprintf('<info>Created output path </info><debug>%s</debug>', $this->outputPath()));
+        }
     }
 
     protected function _after()
@@ -48,6 +60,26 @@ abstract class BaseUnitTest extends Test
     public function makeLogMock()
     {
         return m::mock(Log::class);
+    }
+
+    /**
+     * Returns a directories collection mock
+     *
+     * @return m\MockInterface|Directories
+     */
+    public function makeDirectoriesCollectionMock()
+    {
+        return m::mock(Directories::class);
+    }
+
+    /**
+     * Returns filesytem mock
+     *
+     * @return m\MockInterface|Filesystem
+     */
+    public function makeFilesystemMock()
+    {
+        return m::mock(Filesystem::class);
     }
 
     /**
@@ -73,5 +105,26 @@ abstract class BaseUnitTest extends Test
         }
 
         return $output;
+    }
+
+    /**
+     * Get a filesystem instance
+     *
+     * @return Filesystem
+     */
+    public function getFilesystem()
+    {
+        return new Filesystem();
+    }
+
+    /**
+     * Returns the output directory path
+     *
+     * @return string
+     * @throws \Codeception\Exception\ConfigurationException
+     */
+    public function outputPath()
+    {
+        return Configuration::outputDir();
     }
 }
