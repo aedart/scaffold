917af236dd6872366c7809a021eec49173219eb8
Alin Eugen Deac <aedart@gmail.com>
2016-04-20 07:29:39 +0200

    Add Builder interface

---
+++ src/Contracts/Builders/Builder.php
@@ -0,0 +1,11 @@
+<?php namespace Aedart\Scaffold\Contracts\Builders;
+
+/**
+ * Interface Builder
+ *
+ * @author Alin Eugen Deac <aedart@gmail.com>
+ * @package Aedart\Scaffold\Contracts\Builders
+ */
+interface Builder {
+
+}
