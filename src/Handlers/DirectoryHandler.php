e0d24cc8e66cb7278cdf23e5ca7b27aacdd227e9
Alin Eugen Deac <aedart@gmail.com>
2016-04-20 11:14:30 +0200

    Add Directory Handler
    The concrete implementation of the default directory handler

---
+++ src/Handlers/DirectoryHandler.php
@@ -0,0 +1,84 @@
+<?php namespace Aedart\Scaffold\Handlers;
+
+use Aedart\Scaffold\Contracts\Collections\Directories;
+use Aedart\Scaffold\Contracts\Handlers\DirectoryHandler as DirectoryHandlerInterface;
+use Aedart\Scaffold\Exceptions\CannotCreateDirectoryException;
+
+/**
+ * Directory Handler
+ *
+ * @see \Aedart\Scaffold\Contracts\Collections\Directories
+ *
+ * @author Alin Eugen Deac <aedart@gmail.com>
+ * @package Aedart\Scaffold\Handlers
+ */
+class DirectoryHandler extends BaseHandler implements DirectoryHandlerInterface
+{
+
+    const MODE = 0755;
+
+    /**
+     * Perform the actual element processing
+     *
+     * @param mixed $element
+     *
+     * @return void
+     */
+    public function processElement($element)
+    {
+        /** @var Directories $collection */
+        $collection = $element;
+
+        foreach($collection->all() as $path){
+            $this->createDirectory($path);
+        }
+    }
+
+    /**
+     * Creates the given directory if it does not already exist
+     *
+     * @param string $path
+     *
+     * @throws CannotCreateDirectoryException
+     */
+    protected function createDirectory($path){
+        // Build the final path
+        $finalPath = $this->getOutputPath() . DIRECTORY_SEPARATOR . $path;
+
+        // Get the log and file system
+        $log = $this->getLog();
+        $fs = $this->getFile();
+
+        // Check if already exists
+        if($fs->exists($finalPath)){
+            $log->notice(sprintf('skipped "%s", folder already exists', $finalPath));
+            return;
+        }
+
+        // Create the directory
+        $hasCreated = $fs->makeDirectory($finalPath, self::MODE, true, true);
+
+        // Check if directory was NOT created
+        if(!$hasCreated){
+            throw new CannotCreateDirectoryException(sprintf('Cannot create directory; %s', $finalPath));
+        }
+
+        // Finally, log the output
+        $log->info(sprintf('created "%s"', $finalPath));
+    }
+
+    /**
+     * Creates the given directories inside the output path,
+     * if the directories do not already exist
+     *
+     * @param Directories $collection List of directory paths
+     *
+     * @return void
+     *
+     * @throws CannotCreateDirectoryException
+     */
+    public function processDirectories(Directories $collection)
+    {
+        $this->process($collection);
+    }
+}
