ec1d8e21ee641b83d9b77e93accfb91a300adc18
Alin Eugen Deac <aedart@gmail.com>
2016-04-21 16:02:55 +0200

    Add IoC destroyer test helper trait

---
+++ tests/_testCases/traits/IoCDestroyer.php
@@ -0,0 +1,26 @@
+<?php
+
+use Aedart\Scaffold\Resolvers\IoC;
+
+/**
+ * IoC Destroyer
+ *
+ * @author Alin Eugen Deac <aedart@gmail.com>
+ */
+trait IoCDestroyer
+{
+
+    /**
+     * Destroy the IoC
+     *
+     * This might be needed in some situations where
+     * tests are expected to behave in certain ways,
+     * whenever an IoC has not registered specific
+     * providers or able to resolve instances...
+     */
+    public function destroyIoC()
+    {
+        $ioc = IoC::getInstance();
+        $ioc->destroy();
+    }
+}
