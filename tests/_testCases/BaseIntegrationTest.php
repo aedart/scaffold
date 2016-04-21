cfd901ec210e577bcf44e671ff1f728ff719ec6f
Alin Eugen Deac <aedart@gmail.com>
2016-04-21 16:07:56 +0200

    Add Base Integration Test class
    A base class for all integration tests

---
+++ tests/_testCases/BaseIntegrationTest.php
@@ -0,0 +1,25 @@
+<?php
+
+/**
+ * Base Integration Test
+ *
+ * The core test-class for all integration tests
+ *
+ * @author Alin Eugen Deac <aedart@gmail.com>
+ */
+abstract class BaseIntegrationTest extends BaseUnitTest
+{
+    protected function _before()
+    {
+        parent::_before();
+    }
+
+    protected function _after()
+    {
+        parent::_after();
+    }
+
+    /********************************************************
+     * Helpers
+     *******************************************************/
+}
