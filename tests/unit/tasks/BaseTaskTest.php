6156b934d79234b8840204523708f6998e659af7
Alin Eugen Deac <aedart@gmail.com>
2016-04-21 13:09:38 +0200

    Test resolveHandler method inside BaseTaskTest
    This is an overlap, but not that big of a deal!

--- tests/unit/tasks/BaseTaskTest.php
+++ tests/unit/tasks/BaseTaskTest.php
@@ -1,7 +1,10 @@
 <?php
 
+use Aedart\Scaffold\Handlers\DirectoriesHandler;
 use Aedart\Scaffold\Tasks\BaseTask;
+use Illuminate\Config\Repository;
 use Mockery as m;
+use Symfony\Component\Console\Output\OutputInterface;
 
 /**
  * Class BaseTaskTest
@@ -25,6 +28,19 @@ class BaseTaskTest extends ConsoleTest
         return m::mock(BaseTask::class)->makePartial();
     }
 
+    /**
+     * Returns a new Dummy Task instance
+     *
+     * @param OutputInterface $output
+     * @param Repository $config
+     *
+     * @return DummyTask
+     */
+    public function makeDummyTask(OutputInterface $output, Repository $config)
+    {
+        return new DummyTask($output, $config);
+    }
+
     /********************************************************
      * Actual tests
      *******************************************************/
@@ -42,5 +58,72 @@ class BaseTaskTest extends ConsoleTest
             ->once();
 
         $task->execute($this->makeInputMock(), $this->makeOutputMock(), $this->makeConfigRepositoryMock());
+    }
+
+    /**
+     * @test
+     *
+     * @covers ::resolveHandler
+     *
+     * NOTE: This test is an overlap of the IoCTest::canResolveAndConfigureAHandler !
+     * We only test this here (again), because of (a) code coverage and (b) it is an
+     * important method for many tasks
+     */
+    public function canResolveHandler()
+    {
+        $output = $this->makeOutputMock();
+        $output->shouldReceive('text')
+            ->once()
+            ->with(m::type('string'));
+
+        $config = new Repository([
+            'handlers' => [
+                'directory' => DirectoriesHandler::class
+            ]
+        ]);
+
+        $task = $this->makeDummyTask($output, $config);
+
+        $handler = $task->invokeResolveHandler('handlers.directory');
+
+        $this->assertNotNull($handler);
+    }
+}
+
+/**
+ * Dummy Task
+ *
+ * @author Alin Eugen Deac <aedart@gmail.com>
+ */
+class DummyTask extends BaseTask
+{
+
+    public function __construct(OutputInterface $output, Repository $config)
+    {
+        $this->config = $config;
+        $this->output = $output;
+    }
+
+    /**
+     * Performs the actual task execution
+     *
+     * @return void
+     */
+    public function performTask()
+    {
+        // N/A
+    }
+
+    /**
+     * Exposes the resolveHandler method, so that it
+     * can be tested
+     *
+     * @param $alias
+     *
+     * @return \Aedart\Scaffold\Contracts\Handlers\Handler
+     */
+    public function invokeResolveHandler($alias)
+    {
+        return $this->resolveHandler($alias);
     }
 }
