dfdddd4f8b2402041c0abc8a082857dcfc69867a
Alin Eugen Deac <aedart@gmail.com>
2016-04-22 17:19:31 +0200

    Add configuration file for "copy files" test
    For the build command test

---
+++ tests/_data/commands/build/filesOnly.php
@@ -0,0 +1,18 @@
+<?php
+
+return [
+
+    'name'          => 'Files only',
+
+    'description'   => 'To be used for testing only - test if the build command can copy files',
+
+    'basePath' => __DIR__ . '/resources/',
+
+    'files' => [
+        // Source files (inside 'basePath')  =>  Destination
+        '.gitkeep'                  =>  '.gitkeep',
+        'logs/default.log'          =>  'log.log',
+        'docs/LICENSE.txt'          =>  'LICENSE',
+        'docs/README.md'            =>  'README.md',
+    ],
+];
