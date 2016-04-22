798eb6ef1564fb445ae88bfe72fbd5f85b0ffba0
Alin Eugen Deac <aedart@gmail.com>
2016-04-22 10:35:39 +0200

    Add files collection

---
+++ src/Collections/Files.php
@@ -0,0 +1,68 @@
+<?php namespace Aedart\Scaffold\Collections;
+
+use Aedart\Scaffold\Contracts\Collections\Files as FilesInterface;
+use Aedart\Util\Traits\Collections\PartialCollectionTrait;
+
+/**
+ * Files
+ *
+ * @see \Aedart\Scaffold\Contracts\Collections\Files
+ *
+ * @author Alin Eugen Deac <aedart@gmail.com>
+ * @package Aedart\Scaffold\Collections
+ */
+class Files implements FilesInterface
+{
+    use PartialCollectionTrait;
+
+    /**
+     * Files constructor.
+     *
+     * @param string[][] $files [optional]
+     */
+    public function __construct(array $files = [])
+    {
+        $this->populate($files);
+    }
+
+    public function offsetExists($offset)
+    {
+        return $this->getInternalCollection()->has($offset);
+    }
+
+    public function offsetGet($offset)
+    {
+        return $this->getInternalCollection()->get($offset);
+    }
+
+    public function offsetSet($offset, $value)
+    {
+        $this->put($offset, $value);
+    }
+
+    public function offsetUnset($offset)
+    {
+        $this->getInternalCollection()->forget($offset);
+    }
+
+    public function put($sourceFile, $destination)
+    {
+        $this->getInternalCollection()->put($sourceFile, $destination);
+    }
+
+    public function all()
+    {
+        return $this->getInternalCollection()->all();
+    }
+
+    public function populate(array $data)
+    {
+        if(empty($data)){
+            return;
+        }
+
+        foreach($data as $sourceFile => $destination){
+            $this->put($sourceFile, $destination);
+        }
+    }
+}
