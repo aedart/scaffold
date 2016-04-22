6080d57ef0c3a10239413bee448e8e5bfa222e99
Alin Eugen Deac <aedart@gmail.com>
2016-04-22 10:24:48 +0200

    Add default file handler interface

---
+++ src/Contracts/Handlers/FilesHandler.php
@@ -0,0 +1,27 @@
+<?php namespace Aedart\Scaffold\Contracts\Handlers;
+
+use Aedart\Scaffold\Contracts\Collections\Files;
+use Aedart\Scaffold\Exceptions\CannotCopyFileException;
+
+/**
+ * Files Handler
+ *
+ * @author Alin Eugen Deac <aedart@gmail.com>
+ * @package Aedart\Scaffold\Contracts\Handlers
+ */
+interface FilesHandler
+{
+    /**
+     * Copies the given collection of source files into
+     * their desired destination
+     *
+     * @see Files
+     *
+     * @param Files $collection List of source files and their destinations
+     *
+     * @return void
+     *
+     * @throws CannotCopyFileException
+     */
+    public function processFiles(Files $collection);
+}
---
+++ src/Exceptions/CannotCopyFileException.php
@@ -0,0 +1,17 @@
+<?php namespace Aedart\Scaffold\Exceptions;
+
+use RuntimeException;
+
+/**
+ * Cannot Copy File Exception
+ *
+ * Throw this exception whenever a file cannot be copied into
+ * a specific location
+ *
+ * @author Alin Eugen Deac <aedart@gmail.com>
+ * @package Aedart\Scaffold\Exceptions
+ */
+class CannotCopyFileException extends RuntimeException
+{
+
+}
