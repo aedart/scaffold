eb22c06d2cb34fe79f2adb9f13cd405e09a8c0a3
Alin Eugen Deac <aedart@gmail.com>
2016-04-23 10:32:08 +0200

    Add Task Runner
    Concrete implementation of the console task runner

---
+++ src/Tasks/TaskRunner.php
@@ -0,0 +1,106 @@
+<?php namespace Aedart\Scaffold\Tasks;
+
+use Aedart\Scaffold\Contracts\Tasks\ConsoleTask;
+use Aedart\Scaffold\Contracts\Tasks\ConsoleTaskRunner;
+use Aedart\Scaffold\Exceptions\CannotExecuteTaskException;
+use Illuminate\Contracts\Config\Repository;
+use InvalidArgumentException;
+use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Output\OutputInterface;
+use Symfony\Component\Console\Style\StyleInterface;
+
+/**
+ * Task Runner
+ *
+ * @see \Aedart\Scaffold\Contracts\Tasks\ConsoleTaskRunner
+ *
+ * @author Alin Eugen Deac <aedart@gmail.com>
+ * @package Aedart\Scaffold\Tasks
+ */
+class TaskRunner implements ConsoleTaskRunner
+{
+    public function execute(array $tasks, InputInterface $input, OutputInterface $output, Repository $config)
+    {
+        $i = 1;
+        $total = count($tasks);
+
+        foreach($tasks as $task){
+            // Create new task instance
+            $taskToExecute = $this->initTask($task);
+
+            // Output status info
+            $this->outputExecutionInfo($output, $taskToExecute, $i, $total);
+
+            // Execute the task
+            $taskToExecute->execute($input, $output, $config);
+            $i++;
+        }
+    }
+
+    /**
+     * Init and return the console task
+     *
+     * @param string $taskPath Class path of the task
+     *
+     * @return \Aedart\Scaffold\Contracts\Tasks\ConsoleTask
+     *
+     * @throws CannotExecuteTaskException
+     */
+    protected function initTask($taskPath)
+    {
+        try {
+            $consoleTask = (new $taskPath);
+            $this->assertIsConsoleTask($consoleTask);
+
+            return $consoleTask;
+        } catch (\Exception $e){
+            throw new CannotExecuteTaskException($e->getMessage(), $e->getCode(), $e);
+        }
+    }
+
+    /**
+     * Assert that the given task is an instance of
+     * "console task"
+     *
+     * @see \Aedart\Scaffold\Contracts\Tasks\ConsoleTask
+     *
+     * @param mixed $task
+     *
+     * @throws InvalidArgumentException
+     */
+    protected function assertIsConsoleTask($task)
+    {
+        if(!($task instanceof ConsoleTask)){
+            throw new InvalidArgumentException(
+                sprintf('Cannot execute task, %s is not instance of %s', get_class($task), ConsoleTask::class)
+            );
+        }
+    }
+
+    /**
+     * Output execution status information to the console
+     *
+     * @param OutputInterface|StyleInterface $output
+     * @param ConsoleTask $task
+     * @param int $number The number of the task to be executed
+     * @param int $total The total amount of tasks to be executed
+     */
+    protected function outputExecutionInfo($output, ConsoleTask $task, $number, $total)
+    {
+        $taskXofY = "Task ({$number}/{$total})";
+
+        // Special formatting, if Symfony Style is provided
+        if($output instanceof StyleInterface){
+            $output->section($task->getName());
+            $output->text($taskXofY);
+            $output->newLine();
+            return;
+        }
+
+        // Std. formatting
+        $output->writeln($task->getName());
+        $output->writeln('--------------------------------');
+        $output->writeln($taskXofY);
+        $output->writeln('');
+    }
+}
