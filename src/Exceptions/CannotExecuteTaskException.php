3b141d6ea97d128cc746095c904243573ad5189a
Alin Eugen Deac <aedart@gmail.com>
2016-04-23 10:14:49 +0200

    Add Cannot Execute Task Exception

---
+++ src/Exceptions/CannotExecuteTaskException.php
@@ -0,0 +1,16 @@
+<?php namespace Aedart\Scaffold\Exceptions;
+
+use RuntimeException;
+
+/**
+ * Cannot Execute Task Exception
+ *
+ * Throw this exception whenever a console task cannot be executed
+ *
+ * @author Alin Eugen Deac <aedart@gmail.com>
+ * @package Aedart\Scaffold\Exceptions
+ */
+class CannotExecuteTaskException extends RuntimeException
+{
+
+}
