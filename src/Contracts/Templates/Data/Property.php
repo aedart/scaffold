05fb0ec58a7e85a4373091a3e3cb5962bf9edf27
Alin Eugen Deac <aedart@gmail.com>
2016-04-23 17:44:27 +0200

    Add Template Data Property interface
    NOT yet completed, still very experimental!

---
+++ src/Contracts/Templates/Data/Property.php
@@ -0,0 +1,39 @@
+<?php namespace Aedart\Scaffold\Contracts\Templates\Data;
+
+use Aedart\Model\Contracts\Arrays\ChoicesAware;
+use Aedart\Model\Contracts\Integers\IdAware;
+use Aedart\Model\Contracts\Integers\TypeAware;
+use Aedart\Model\Contracts\Strings\QuestionAware;
+use Aedart\Model\Contracts\Strings\ValueAware;
+use Aedart\Util\Interfaces\Populatable;
+use ArrayAccess;
+use Illuminate\Contracts\Support\Arrayable;
+use Illuminate\Contracts\Support\Jsonable;
+use JsonSerializable;
+
+/**
+ * Template Data Property
+ *
+ * TODO: Desc...
+ *
+ * @author Alin Eugen Deac <aedart@gmail.com>
+ * @package Aedart\Scaffold\Contracts\Templates\Data
+ */
+interface Property extends IdAware,
+    TypeAware,
+    QuestionAware,
+    ChoicesAware,
+    ValueAware,
+
+    // TODO: validate aware
+    // TODO: max Attempts aware
+    // TODO: post Process aware
+
+    ArrayAccess,
+    Arrayable,
+    Jsonable,
+    JsonSerializable,
+    Populatable
+{
+
+}
