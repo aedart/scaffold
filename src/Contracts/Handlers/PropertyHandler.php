e24a792ce62777111cbb36068d09eb7b29abdd35
Alin Eugen Deac <aedart@gmail.com>
2016-04-24 09:53:24 +0200

    Add Template Data Property handler interface

---
+++ src/Contracts/Handlers/PropertyHandler.php
@@ -0,0 +1,27 @@
+<?php namespace Aedart\Scaffold\Contracts\Handlers;
+
+use Aedart\Scaffold\Contracts\Templates\Data\Property;
+use Aedart\Scaffold\Exceptions\CannotProcessPropertyException;
+
+/**
+ * Property Handler
+ *
+ * @author Alin Eugen Deac <aedart@gmail.com>
+ * @package Aedart\Scaffold\Contracts\Handlers
+ */
+interface PropertyHandler
+{
+    /**
+     * Process the given template data property. If needed,
+     * the method will ask the user for a value.
+     *
+     * @see Property
+     *
+     * @param Property $property Some kind of a template data property
+     *
+     * @return void
+     *
+     * @throws CannotProcessPropertyException
+     */
+    public function processProperty(Property $property);
+}
---
+++ src/Exceptions/CannotProcessPropertyException.php
@@ -0,0 +1,17 @@
+<?php namespace Aedart\Scaffold\Exceptions;
+
+use RuntimeException;
+
+/**
+ * Cannot Process Property Exception
+ *
+ * Throw this exception whenever a template data property cannot
+ * be processed
+ *
+ * @author Alin Eugen Deac <aedart@gmail.com>
+ * @package Aedart\Scaffold\Exceptions
+ */
+class CannotProcessPropertyException extends RuntimeException
+{
+
+}
