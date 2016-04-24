d6858a11a42cc2ee120c369f4ab772fbe51ec6f2
Alin Eugen Deac <aedart@gmail.com>
2016-04-24 10:52:29 +0200

    Use property handler in Task
    The property processing logic has now moved completely inside the
    property handler. The task now only reads the configuration entries
    and passes them on to the handler.

--- src/Tasks/AskForTemplateData.php
+++ src/Tasks/AskForTemplateData.php
@@ -2,9 +2,7 @@
 
 use Aedart\Scaffold\Containers\IoC;
 use Aedart\Scaffold\Contracts\Collections\TemplateProperties;
-use Aedart\Scaffold\Contracts\Templates\Data\Property;
-use Aedart\Scaffold\Contracts\Templates\Data\Type;
-use Illuminate\Contracts\Config\Repository;
+use Aedart\Scaffold\Contracts\Handlers\PropertyHandler;
 
 /**
  * Ask For Template Data Task
@@ -15,7 +13,7 @@ use Illuminate\Contracts\Config\Repository;
 class AskForTemplateData extends BaseTask
 {
 
-    protected $description = 'Processes template data';
+    protected $description = 'Processes template data properties';
 
     /**
      * Performs the actual task execution
@@ -36,58 +34,16 @@ class AskForTemplateData extends BaseTask
 
         // Process the properties
         foreach($collection->all() as $id => $property){
-            // Process the value (ask for it if needed)
-            $value = $this->processProperty($property);
+            // Create the key, where the processed value
+            // will be stored (inside the configuration).
+            $key = $templateDataKey . '.' . $id . '.value';
 
-            // Output what value is being used
-            // TODO: Do NOT display value, if type is hidden!
-            // TODO: Display 'true / false' for boolean values
-            $this->output->text("Using <info>{$value}</info> for <comment>{$id}</comment>");
+            // Get the handler
+            $handler = $this->getPropertyHandler($key);
+
+            // Process the property
+            $handler->processProperty($property);
         }
-    }
-
-    // TODO: Implement, test and PHPDoc...
-    protected function processProperty(Property $property)
-    {
-        $type = $property->getType();
-        $finalValue = null;
-
-        switch($type){
-            case Type::VALUE:
-                $finalValue = $property->getValue();
-                break;
-
-            case Type::QUESTION:
-                // TODO: Implement validation
-                $finalValue = $this->output->ask($property->getQuestion(), $property->getValue(), null);
-                break;
-
-            case Type::CHOICE:
-                $finalValue = $this->output->choice($property->getQuestion(), $property->getChoices(), $property->getValue());
-                break;
-
-            case Type::CONFIRM:
-                $finalValue = $this->output->confirm($property->getQuestion(), $property->getValue());
-                break;
-
-            case Type::HIDDEN:
-                // TODO: Implement validation
-
-                // TODO: Hidden should always be confirmed - usage most likely for passwords...
-
-                $finalValue = $this->output->askHidden($property->getQuestion(), null);
-                break;
-
-            default:
-                // TODO: Fail here...
-                $this->output->warning("throw exception for '{$property->getId()}', type {$property->getType()} is unsupported");
-                break;
-        }
-
-        // TODO: Implement "post process" of value
-
-        // Finally, return the final value
-        return $finalValue;
     }
 
     /**
@@ -102,5 +58,24 @@ class AskForTemplateData extends BaseTask
         $ioc = IoC::getInstance();
 
         return $ioc->make(TemplateProperties::class, $properties);
+    }
+
+    /**
+     * Get the property handler
+     *
+     * @see \Aedart\Scaffold\Handlers\PropertyHandler
+     *
+     * @param string $key The "key" or "index" inside the configuration repository,
+     *                           in which the final processed value must be stored.
+     *
+     * @return PropertyHandler
+     */
+    protected function getPropertyHandler($key)
+    {
+        return $this->resolveHandler('handlers.property', [
+            'config'    => $this->config,
+            'key'       => $key,
+            'output'    => $this->output
+        ]);
     }
 }
