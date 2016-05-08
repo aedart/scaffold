44d9003bce6140252aa0d41cfeb0f052de9b6432
Alin Eugen Deac <aedart@gmail.com>
2016-05-08 12:14:09 +0200

    Add template interface

---
+++ src/Contracts/Templates/Template.php
@@ -0,0 +1,20 @@
+<?php namespace Aedart\Scaffold\Contracts\Templates;
+
+use Aedart\Model\Contracts\Strings\IdAware;
+use Aedart\Model\Contracts\Strings\SourceAware;
+
+/**
+ * Template
+ *
+ * This component represents some kind of a physical template.
+ * It consists of a source location, a destination and an ID.
+ *
+ * @author Alin Eugen Deac <aedart@gmail.com>
+ * @package Aedart\Scaffold\Contracts\Templates
+ */
+interface Template extends IdAware,
+    SourceAware,
+    DestinationAware
+{
+
+}
