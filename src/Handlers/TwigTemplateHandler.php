7b2ee220660b497d03d651e714941f59804a46fc
Alin Eugen Deac <aedart@gmail.com>
2016-05-10 20:45:01 +0200

    Add generate file method(s)
    Handler should now be able to generate a file, based on a given
    template, at a the given destination, with any eventual template data
    properties assigned to it.
    At this point, it still needs to be tested!

--- src/Handlers/TwigTemplateHandler.php
+++ src/Handlers/TwigTemplateHandler.php
@@ -1,14 +1,24 @@
 <?php namespace Aedart\Scaffold\Handlers;
 
+use Aedart\Scaffold\Contracts\Collections\TemplateProperties;
 use Aedart\Scaffold\Contracts\Handlers\TemplateHandler;
 use Aedart\Scaffold\Contracts\Templates\Template;
 use Aedart\Scaffold\Exceptions\CannotProcessTemplateException;
 use Aedart\Scaffold\Templates\TemplateData;
+use Exception;
 use Twig_Environment;
 use Twig_Loader_Filesystem;
 
 /**
- * Class TwigTemplateHandler
+ * Twig Template Handler
+ *
+ * This handler generates files from a given template, based on
+ * the Twig Template Engine
+ *
+ * @see \Aedart\Scaffold\Contracts\Handlers\TemplateHandler
+ * @see \Twig_Environment
+ *
+ * @see http://twig.sensiolabs.org/
  *
  * @author Alin Eugen Deac <aedart@gmail.com>
  * @package Aedart\Scaffold\Handlers
@@ -54,25 +64,106 @@ class TwigTemplateHandler extends BaseHandler implements TemplateHandler
     /**
      * Render the given template
      *
-     * @param Template $template
+     * @param string $templatePath Path to the template
      * @param array $data [optional] Context data to be assigned to the template
      *
      * @return string
      */
-    protected function renderTemplate(Template $template, array $data = [])
+    protected function renderTemplate($templatePath, array $data = [])
     {
         return $this->engine
-            ->loadTemplate($this->getBasePath() . $template->getSource())
+            ->loadTemplate($templatePath)
             ->render($data);
     }
 
+    /**
+     * Parse the given collection into an array, which can be
+     * assigned to a template.
+     *
+     * @param TemplateProperties $collection
+     *
+     * @return array
+     */
+    protected function parseTemplateDataToArray(TemplateProperties $collection)
+    {
+        $output = [];
+
+        foreach($collection->all() as $id => $property){
+            $output[$id] = $property->getValue();
+        }
+
+        return $output;
+    }
+
+    /**
+     * Generate a file based on the given template.
+     *
+     * If this handler has any template data set, it will
+     * be assigned to the given template.
+     *
+     * @see Template
+     * @see getTemplateData()
+     *
+     * @param Template $template The template to use for generating a file
+     *
+     * @throws CannotProcessTemplateException If (a) template's source does not exist,
+     * (b) a file already exists at the template's destination, or (c) unable to
+     * generate the file.
+     */
     protected function generateFileFrom(Template $template)
     {
-        // TODO: Fail if source does not exist!
-        // TODO: Fail if destination does exist!
-        // TODO: Fetch the template data - if any available...
-        // TODO: Try catch render
-        // TODO: Persist the compiled file
+        // Set a few variables...
+        $fs = $this->getFile();
+        $templatePath = $this->getBasePath() . $template->getSource();
+        $destination = $this->outputPath . $template->getDestination()->getValue();
+        $data = [];
+
+        // Fail if source template does not exist
+        if(!$fs->exists($templatePath)){
+            throw new CannotProcessTemplateException(sprintf('Template %s does not exist', $templatePath));
+        }
+
+        // Fail if destination file already exists
+        if($fs->exists($destination)){
+            throw new CannotProcessTemplateException(sprintf('File %s already exists, will not overwrite!', $destination));
+        }
+
+        // Parse template data properties to array, if any available
+        if($this->hasTemplateData()){
+            $data = $this->parseTemplateDataToArray($this->getTemplateData());
+        }
+
+        // Finally, generate the file...
+        $this->generateFile($templatePath, $destination, $data);
+    }
+
+    /**
+     * Generates a file at given destination, based on the
+     * given template and context data
+     *
+     * @param string $templatePath Path to the template
+     * @param string $destination Location and file name to be generated
+     * @param array $data [optional] Context data to be assigned to the template
+     *
+     * @throws CannotProcessTemplateException If unable to render template or write file to
+     * the disk
+     */
+    protected function generateFile($templatePath, $destination, array $data = [])
+    {
+        try {
+            // Render the template
+            $content = $this->renderTemplate($templatePath, $data);
+
+            // Write the file
+            $bytes = $this->getFile()->append($destination, $content);
+
+            // Check if file was written
+            if($bytes === false){
+                throw new \RuntimeException(sprintf('Unable to write %s to the disk. Please check your permissions!', $destination));
+            }
+        } catch (Exception $e){
+            throw new CannotProcessTemplateException($e->getMessage(), $e->getCode(), $e);
+        }
     }
 
     /**
