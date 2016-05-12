aeb5ec103604f36be964f02584fb0891310232f1
Alin Eugen Deac <aedart@gmail.com>
2016-05-12 20:17:40 +0200

    Apply template handler in task
    Completed the concrete implementation of the
    "Generate Files From Templates" task.

--- src/Tasks/GenerateFilesFromTemplates.php
+++ src/Tasks/GenerateFilesFromTemplates.php
@@ -1,6 +1,9 @@
 <?php namespace Aedart\Scaffold\Tasks;
 
+use Aedart\Scaffold\Collections\Utility\PropertiesCollectionParser;
 use Aedart\Scaffold\Collections\Utility\TemplateCollectionParser;
+use Aedart\Scaffold\Contracts\Collections\TemplateProperties;
+use Aedart\Scaffold\Contracts\Handlers\TemplateHandler;
 
 /**
  * Generate Files From Templates
@@ -10,9 +13,10 @@ use Aedart\Scaffold\Collections\Utility\TemplateCollectionParser;
  */
 class GenerateFilesFromTemplates extends BaseTask
 {
+    use PropertiesCollectionParser;
     use TemplateCollectionParser;
 
-    protected $description = 'Generate files from the template(s)';
+    protected $description = 'Builds files based on templates';
 
     /**
      * Performs the actual task execution
@@ -21,6 +25,9 @@ class GenerateFilesFromTemplates extends BaseTask
      */
     public function performTask()
     {
+        // Get the properties collection
+        $templateData = $this->getTemplateData();
+
         // Check if there are any templates that must be compiled into files...
         $templateKey = 'templates';
         if (!$this->config->has($templateKey) || count($this->config->get($templateKey, [])) == 0) {
@@ -31,26 +38,45 @@ class GenerateFilesFromTemplates extends BaseTask
         // Get the templates collection
         $collection = $this->parseTemplatesCollection($this->config->get($templateKey));
 
+        // Fetch the template handler
+        $handler = $this->makeTemplateHandler($templateData);
+
         // Process the "templates" - generate files!
         foreach ($collection->all() as $id => $template) {
 
-            // TODO: Fetch handler and process the template...
-
-//            // Get the destination property
-//            $property = $template->getDestination();
-//
-//            // Set the property's id!
-//            $property->setId($id);
-//
-//            // Create the key, where the processed value
-//            // will be stored (inside the configuration).
-//            $key = $templateKey . '.' . $id . '.destination.value';
-//
-//            // Get the handler
-//            $handler = $this->makePropertyHandler($key);
-//
-//            // Process the property
-//            $handler->processProperty($property);
+            // Process the given template
+            $handler->processTemplate($template);
         }
+    }
+
+    /**
+     * Get the template handler
+     *
+     * @param TemplateProperties $templateData
+     *
+     * @return TemplateHandler
+     */
+    public function makeTemplateHandler(TemplateProperties $templateData)
+    {
+        return $this->resolveHandler('handlers.template', [
+            'templateData'    => $templateData,
+        ]);
+    }
+
+    /**
+     * Get any eventual template data properties
+     *
+     * @return \Aedart\Scaffold\Contracts\Collections\TemplateProperties
+     */
+    protected function getTemplateData()
+    {
+        // Attempt to obtain eventual template data
+        $templateDataKey = 'templateData';
+        if(!$this->config->has($templateDataKey) || count($this->config->get($templateDataKey, [])) == 0){
+            $this->output->note('No template data for templates');
+        }
+
+        // Get the properties collection
+        return $this->parsePropertiesCollection($this->config->get($templateDataKey));
     }
 }
