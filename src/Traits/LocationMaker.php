29a706f5da2c5c647fb752ba3288e6fb867ac038
Alin Eugen Deac <aedart@gmail.com>
2016-05-22 18:07:16 +0200

    Add a scaffold location maker utility

---
+++ src/Traits/LocationMaker.php
@@ -0,0 +1,25 @@
+<?php namespace Aedart\Scaffold\Traits;
+
+use Aedart\Scaffold\Containers\IoC;
+use Aedart\Scaffold\Contracts\Indexes\ScaffoldLocation;
+
+/**
+ * Scaffold Location Maker
+ *
+ * @author Alin Eugen Deac <aedart@gmail.com>
+ * @package Aedart\Scaffold\Traits
+ */
+trait LocationMaker
+{
+    /**
+     * Returns a new Scaffold Location object
+     *
+     * @param array $data [optional]
+     *
+     * @return ScaffoldLocation
+     */
+    public function makeLocation(array $data = [])
+    {
+        return IoC::getInstance()->make(ScaffoldLocation::class, $data);
+    }
+}
