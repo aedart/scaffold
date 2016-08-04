7fe53360af2afd03e826ed47f630dfb14e8a489d
Alin Eugen Deac <aedart@gmail.com>
2016-08-04 18:42:47 +0200

    Add custom cannot populate for index

---
+++ src/Exceptions/CannotPopulateIndexException.php
@@ -0,0 +1,15 @@
+<?php
+namespace Aedart\Scaffold\Exceptions;
+
+use InvalidArgumentException;
+
+/**
+ * CannotPopulateIndexException
+ *
+ * @author Alin Eugen Deac <aedart@gmail.com>
+ * @package Aedart\Scaffold\Exceptions
+ */
+class CannotPopulateIndexException extends InvalidArgumentException
+{
+
+}
