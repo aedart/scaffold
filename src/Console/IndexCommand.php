39727dfe522565dbdb0fa99c15a355b56f5b16fc
Alin Eugen Deac <aedart@gmail.com>
2016-08-07 14:25:46 +0200

    Add directories and force option
    Can now build index files, based on found scaffold files in the
    given directories. Also possible to force indexing.

--- src/Console/IndexCommand.php
+++ src/Console/IndexCommand.php
@@ -2,6 +2,8 @@
 
 use Aedart\Laravel\Helpers\Traits\Filesystem\FileTrait;
 use Aedart\Scaffold\Indexes\IndexBuilder;
+use Composer\Factory;
+use Symfony\Component\Console\Input\InputOption;
 
 /**
  * Index Command
@@ -19,7 +21,8 @@ class IndexCommand extends BaseCommand
     {
         $this
             ->setName('index')
-            ->setDescription('TODO');
+            ->setDescription('TODO')
+            ->addOption('force', 'f', InputOption::VALUE_NONE, 'Force build a new index file');
             //->addArgument('config', InputArgument::REQUIRED, 'Path to the scaffold configuration file')
             //->addOption('output', null, InputOption::VALUE_OPTIONAL, 'Path where to build project or resource', getcwd())
             //->setHelp($this->formatHelp());
@@ -32,11 +35,39 @@ class IndexCommand extends BaseCommand
      */
     public function runCommand()
     {
-        $builder = new IndexBuilder();
+        $this->output->title('Building index');
 
-        $builder->build();
+        $builder = new IndexBuilder($this->output);
 
-        // TODO: Remove this...
-        $this->output->success('Index command...');
+        $builder->build($this->directories(), $this->input->getOption('force'));
+
+        $this->output->success('Indexing completed');
+    }
+
+    /**
+     * Returns a list of directory paths in which scaffold files
+     * are to be searched for, in order to build an index
+     *
+     * @return string[]
+     */
+    public function directories()
+    {
+        $composerConfig = Factory::createConfig(null, getcwd());
+
+        $vendorDir = $composerConfig->get('vendor-dir');
+        $globalVendorDir = (Factory::createConfig(null, $composerConfig->get('home')))->get('vendor-dir');
+
+        return [
+
+            // The current working directory of where this command
+            // is being executed from
+            getcwd() . DIRECTORY_SEPARATOR,
+
+            // The vendor folder inside the current working directory
+            $vendorDir,
+
+            // The "global" vendor directory inside the composer home
+            $globalVendorDir
+        ];
     }
 }
