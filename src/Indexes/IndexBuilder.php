e782ca17ee522a1c399b0c565b97bbd55ca6ca5d
Alin Eugen Deac <aedart@gmail.com>
2016-08-07 14:24:15 +0200

    Re-implement index builder
    Still not complete, but it is starting to work well (manual tests).

--- src/Indexes/IndexBuilder.php
+++ src/Indexes/IndexBuilder.php
@@ -1,9 +1,15 @@
 <?php namespace Aedart\Scaffold\Indexes;
 
 use Aedart\Laravel\Helpers\Traits\Filesystem\FileTrait;
+use Aedart\Scaffold\Contracts\Indexes\Index;
 use Aedart\Scaffold\Traits\IndexDirectoryPath;
+use Aedart\Scaffold\Traits\IndexMaker;
+use Aedart\Scaffold\Traits\LocationMaker;
+use Aedart\Scaffold\Traits\OutputHelper;
 use Carbon\Carbon;
 use Illuminate\Support\Str;
+use Symfony\Component\Console\Style\StyleInterface;
+use Symfony\Component\Finder\Finder;
 
 /**
  * Index Builder
@@ -17,6 +23,9 @@ class IndexBuilder
 {
     use FileTrait;
     use IndexDirectoryPath;
+    use LocationMaker;
+    use IndexMaker;
+    use OutputHelper;
 
     /**
      * Name of the directory where
@@ -31,9 +40,14 @@ class IndexBuilder
     const SCAFFOLD_FILE_PATTERN = '*.scaffold.php';
 
     /**
-     * The index content
+     * Name of index file
+     */
+    const DEFAULT_INDEX_FILE_NAME = 'index.json';
+
+    /**
+     * The index
      *
-     * @var array
+     * @var Index
      */
     protected $index = [];
 
@@ -47,25 +61,46 @@ class IndexBuilder
 
     /**
      * IndexBuilder constructor.
+     *
+     * @param StyleInterface $output [optional]
      */
-    public function __construct()
+    public function __construct(StyleInterface $output = null)
     {
-        // TODO: Needs a getter and setter - and perhaps accept this via constructor (optional)
-        $this->placesToSearch = [
-            getcwd() . DIRECTORY_SEPARATOR,
-            // TODO: vendor path, ==> $composerConfig = \Composer\Factory::createConfig(null, getcwd());
-            // TODO: global vendor path
-        ];
+        // Set eventual output
+        $this->setOutput($output);
+
+        // Make a new empty index
+        $this->index = $this->makeIndex();
     }
 
     /**
      * Searches and generates an index file of all the found
      * scaffold configuration files
+     *
+     * @see Index::hasExpired()
+     *
+     * @param string[] $directories Paths where to search for scaffold files
+     * @param bool $force [optional] If true, an index file will be build, regardless of expiration date
+     *
+     * @return Index Either a new index is build or a cached version is used
      */
-    public function build()
+    public function build(array $directories, $force = false)
     {
-        // TODO: $force option
-        // TODO: do we really need to build - check if exists and if expired...
+        // Get the path to the index file
+        $path = $this->getPathToIndexFile();
+
+        // Check if we really need to build a new index
+        if(!$force && $this->doesIndexFileExist()){
+            $index = $this->loadIndexFromFile($path);
+
+            // If the index has not expired, then return the index
+            if(!$index->hasExpired()){
+                $this->outputNote('Using cached index (' . $path . ')');
+                return $index;
+            }
+        }
+
+        $this->outputNote('Building new index (' . $path . ')');
 
         // Create scaffold directory if not already exists
         $this->makeIndexDirectory();
@@ -74,12 +109,60 @@ class IndexBuilder
         $this->gitIgnoreIndexDirectory();
 
         // Search for scaffold files and add them to the index
-        foreach($this->placesToSearch as $path){
+        foreach($directories as $path){
             $this->index($path);
         }
 
         // Build the index file
         $this->buildIndexFile($this->index);
+
+        // Finally, return the index
+        return $this->index;
+    }
+
+    /**
+     * Check if an index file already exists
+     *
+     * @return bool
+     */
+    protected function doesIndexFileExist()
+    {
+        return $this->getFile()->exists($this->getPathToIndexFile());
+    }
+
+    /**
+     * Returns the path to the index file
+     *
+     * @return string
+     */
+    protected function getPathToIndexFile()
+    {
+        return $this->getIndexDirectoryPath() . self::DEFAULT_INDEX_FILE_NAME;
+    }
+
+    /**
+     * Removes eventual existing index file
+     */
+    protected function deleteIndexFile()
+    {
+        if($this->doesIndexFileExist()){
+            $this->getFile()->delete($this->getPathToIndexFile());
+        }
+    }
+
+    /**
+     * Returns a new Index instance, populated with the content
+     * from the given index file
+     *
+     * @param string $pathToIndexFile
+     *
+     * @return Index
+     */
+    protected function loadIndexFromFile($pathToIndexFile)
+    {
+        $content = $this->getFile()->get($pathToIndexFile);
+
+        return $this->makeIndex(json_decode($content, true));
     }
 
     /**
@@ -90,20 +173,21 @@ class IndexBuilder
      */
     protected function index($path)
     {
-        $fs = $this->getFile();
+        $this->outputMessage('Searching in ' . $path);
 
         // Search for *.scaffold.php files
-        $scaffolds = $fs->glob( $path . self::SCAFFOLD_FILE_PATTERN);
+        $finder = new Finder();
+        $finder->files()->name(self::SCAFFOLD_FILE_PATTERN)->in($path)->depth('< 3');
 
         // Abort if nothing found
-        if(empty($scaffolds)){
-            // TODO: output if nothing found
+        if($finder->count() == 0){
+            $this->outputNote('No scaffolds found in ' . $path);
             return;
         }
 
         // Add each scaffold to the index
-        foreach($scaffolds as $scaffoldFile){
-            $this->addScaffoldFileToIndex($scaffoldFile);
+        foreach($finder as $file){
+            $this->addScaffoldFileToIndex($file->getRealPath());
         }
     }
 
@@ -116,14 +200,19 @@ class IndexBuilder
      */
     protected function addScaffoldFileToIndex($file)
     {
+        $this->outputNote('Indexing ' . $file);
+
         $fs = $this->getFile();
 
         // Load the scaffold file
         $scaffold = $this->loadScaffoldFile($file);
 
-        // Get the name and desc. of the scaffold
-        $scaffoldName = $scaffold['name'];
-        $scaffoldDescription = $scaffold['description'];
+        // Create a new location instance
+        $location = $this->makeLocation();
+
+        // Get and set the name and desc. of the scaffold
+        $location->setName($scaffold['name']);
+        $location->setDescription($scaffold['description']);
 
         // Default Package vendor and package name
         $packageVendor = '(no vendor)';
@@ -139,40 +228,31 @@ class IndexBuilder
             $packageName = $parts[1];
         }
 
-        // Add to index
-        $this->index[$packageVendor][$packageName][] = [
-            'name'          => $scaffoldName,
-            'description'   => $scaffoldDescription,
-            'file'          => $file
-        ];
+        $location->setVendor($packageVendor);
+        $location->setPackage($packageName);
+
+        // Set the file
+        $location->setFilePath($file);
+
+        // Finally, add the location to the index
+        $this->index->register($location);
     }
 
     /**
-     * Persists the content given into an index file
+     * Persists the index into a file
      *
      * Previous index file will be removed before new
      * index file is saved to the disk
      *
-     * @param array $content
+     * @param Index $index
      */
-    protected function buildIndexFile(array $content)
+    protected function buildIndexFile(Index $index)
     {
-        $fs = $this->getFile();
-
-        // Add expires date
-        $content['expiresAt'] = (string) Carbon::now()->addMinutes(30);
-
-        // Path of the index file
-        // TODO: ... uhm, name of the index file...
-        $indexFile = $this->getIndexDirectoryPath() . 'index.json';
-
         // Clear any eventual previous index file
-        if($fs->exists($indexFile)){
-            $fs->delete($indexFile);
-        }
+        $this->deleteIndexFile();
 
         // Write the index file
-        $fs->append($indexFile, json_encode($content, JSON_PRETTY_PRINT));
+        $this->getFile()->append($this->getPathToIndexFile(), $index->toJson(JSON_PRETTY_PRINT));
     }
 
     /**
