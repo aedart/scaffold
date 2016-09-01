3d9ba63757d4bb82c9a93428b8f80ef7bffccc2c
Alin Eugen Deac <aedart@gmail.com>
2016-09-01 18:40:16 +0200

    Add traits test-case

---
+++ tests/_testCases/TraitsTestCase.php
@@ -0,0 +1,13 @@
+<?php
+
+use Aedart\Testing\GST\GetterSetterTraitTester;
+
+/**
+ * Traits TestCase
+ *
+ * @author Alin Eugen Deac <aedart@gmail.com>
+ */
+abstract class TraitsTestCase extends BaseUnitTest
+{
+    use GetterSetterTraitTester;
+}
