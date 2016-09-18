ed9ffcee9f09c33e5af3dea58ee290dc6865ea06
Alin Eugen Deac <aedart@gmail.com>
2016-09-18 15:59:05 +0200

    Change scaffold, use cache util to test indexing
    If caching isn't configured correctly, then is will crash the test.

--- tests/_data/commands/index/vendorC/soylent/util/utilB.scaffold.php
+++ tests/_data/commands/index/vendorC/soylent/util/utilB.scaffold.php
@@ -4,5 +4,10 @@ return [
 
     'name'          => 'Utility B',
 
-    'description'   => 'Courage ho! loot to be loved.',
+    // Don't know why someone would cache a scaffolds description, yet
+    // we need to ensure that the index can handle this, since the entire array
+    // it processed!
+    'description'   => scaffold_cache()->remember('specialDescription', 1, function(){
+        return 'The bilge rat commands with grace, drink the pacific ocean before it hobbles.';
+    }),
 ];
