b39d2719d994d455bfe1a6b2eee21d1ab3dfda7c
Alin Eugen Deac <aedart@gmail.com>
2016-09-18 11:48:44 +0200

    Add IndexServiceProvider

---
+++ src/Providers/IndexServiceProvider.php
@@ -0,0 +1,68 @@
+<?php
+namespace Aedart\Scaffold\Providers;
+
+use Aedart\Scaffold\Contracts\Builders\IndexBuilder as IndexBuilderInterface;
+use Aedart\Scaffold\Contracts\Indexes\Index;
+use Aedart\Scaffold\Contracts\Indexes\ScaffoldLocation as ScaffoldLocationInterface;
+use Aedart\Scaffold\Indexes\IndexBuilder;
+use Aedart\Scaffold\Indexes\Location;
+use Aedart\Scaffold\Indexes\ScaffoldIndex;
+use Illuminate\Contracts\Container\BindingResolutionException;
+use Illuminate\Support\ServiceProvider;
+/**
+ * Index Service Provider
+ *
+ * @author Alin Eugen Deac <aedart@gmail.com>
+ * @package Aedart\Scaffold\Providers
+ */
+class IndexServiceProvider extends ServiceProvider
+{
+    /**
+     * Register the service provider.
+     *
+     * @return void
+     */
+    public function register()
+    {
+        $this->registerScaffoldLocation();
+        $this->registerScaffoldIndex();
+        $this->registerIndexBuilder();
+    }
+
+    /**
+     * Register the scaffold location object
+     */
+    protected function registerScaffoldLocation()
+    {
+        $this->app->bind(ScaffoldLocationInterface::class, function($app, array $data = []){
+            return new Location($data, $app);
+        });
+    }
+
+    /**
+     * Register the scaffold index object
+     */
+    protected function registerScaffoldIndex()
+    {
+        $this->app->bind(Index::class, function($app, array $data = []){
+            return new ScaffoldIndex($data);
+        });
+    }
+
+    /**
+     * Register the index builder
+     */
+    protected function registerIndexBuilder()
+    {
+        $this->app->bind(IndexBuilderInterface::class, function($app, array $data = []){
+            if(!isset($data['output'])){
+                $target = IndexBuilderInterface::class;
+                $msg = "Target {$target} cannot be build. Missing arguments; e.g. ['output' => (StyleInterface)]";
+
+                throw new BindingResolutionException($msg);
+            }
+
+            return new IndexBuilder($data['output']);
+        });
+    }
+}
